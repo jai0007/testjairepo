33a374fbc82060622ca3defd26d9d9889ea66680
jai0007 <49018847+jai0007@users.noreply.github.com>
2024-12-09 10:15:04 +0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from dash import Dash, html
 
+# Flask instance
 server = Flask(__name__)
 
-@server.route("/")
-def home():
-    return "Hello, Vercel!"
+# Dash instance
+app = Dash(__name__, server=server)
+app.layout = html.Div("Hello, Vercel! Jai here")
 
+# Ensure the app runs on port 8080 for local testing
 if __name__ == "__main__":
-    server.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=8080)
